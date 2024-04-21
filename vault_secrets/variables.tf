bd4114eff9813019ae628ce5d4037f5ed06afc29
caidam <constant.aidam@proton.me>
2024-04-21 14:47:50 +0200

    no default

--- vault_secrets/variables.tf
+++ vault_secrets/variables.tf
@@ -12,5 +12,5 @@ variable "data_json" {
 variable "token_ttl" {
   description = "The Time To Live period of the token. This is specified as a numeric string with suffix like '30s' or '5m'"
   type        = string
-  default     = "768h"
+  # default     = "768h"
 }
